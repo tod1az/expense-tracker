c071425ce5261619d7a9c45913ec1847c29c7ab7
tod1az <t.diaz.soto@gmail.com>
2024-02-29 18:41:51 -0300

    add card component

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,21 +1,25 @@
 import { Text, View } from "react-native";
 import { api } from "../convex/_generated/api";
 import { useQuery } from "convex/react";
+import Card from "./Card";
+import Separator from "./Separator";
+import { getTotalExpenses } from "../lib/helpers";
 
 export default function Main() {
   const expenses = useQuery(api.expenses.getExpenses);
-
   return (
     <View>
-      <Text className="font-bold text-2xl mb-10 text-white pt-6">Expenses</Text>
-      <Text>{}</Text>
+      <Text className="font-bold text-2xl mb-10 text-white text-center pt-6">
+        Gastos
+      </Text>
+      <Separator />
       {expenses?.map((expense) => (
         <View key={expense._id}>
-          <Text className="capitalize font-bold">{expense.description}</Text>
-          <Text>{expense.cost}</Text>
-          <Text>{new Date(expense._creationTime).toLocaleDateString()}</Text>
+          <Card expense={expense} />
+          <Separator />
         </View>
       ))}
+      {expenses && <Text>{`Total:$${getTotalExpenses(expenses)}`}</Text>}
     </View>
   );
 }
