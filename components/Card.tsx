92904b211a77c201005f9a795a14263e83752acf
tod1az <t.diaz.soto@gmail.com>
2024-02-29 18:41:40 -0300

    add card component

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { Expense } from "../lib/types";
+
+type CardProps = {
+  expense: Expense;
+};
+
+export default function Card(props: CardProps) {
+  return (
+    <View className="space-y-1 px-2 w-[300px]">
+      <Text className="capitalize font-bold">{props.expense.description}</Text>
+      <Text>{`$${props.expense.cost}`}</Text>
+      <Text>{new Date(props.expense._creationTime).toLocaleDateString()}</Text>
+    </View>
+  );
+}
