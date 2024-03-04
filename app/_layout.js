1056fb854c938a9b024089b24c1f526b7c178765
tod1az <t.diaz.soto@gmail.com>
2024-03-04 19:52:01 -0300

    add expo router

--- app/_layout.js
+++ app/_layout.js
@@ -1,19 +1,16 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import Main from "./components/Main";
 import { CONVEX_URL } from "@env";
 import "react-native-get-random-values";
-import { View } from "react-native";
+import { Slot } from "expo-router";
 
 const convex = new ConvexReactClient(CONVEX_URL, {
   unsavedChangesWarning: false,
 });
 
-export default function App() {
+export default function Layout() {
   return (
     <ConvexProvider client={convex}>
-      <View className="flex flex-col bg-gray-600 h-screen justify-start items-center text-4xl mt-14">
-        <Main />
-      </View>
+      <Slot />
     </ConvexProvider>
   );
 }
