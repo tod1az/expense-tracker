9565f603918759c6cc0777d3d83bfea019273136
tod1az <t.diaz.soto@gmail.com>
2024-03-04 19:52:25 -0300

    add save expense mutation

--- convex/expenses.ts
+++ convex/expenses.ts
@@ -1,8 +1,19 @@
-import { query } from "./_generated/server";
-
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
 export const getExpenses = query({
   handler: async (ctx) => {
     const expenses = await ctx.db.query("expenses").collect();
     return expenses;
+  },
+});
+
+export const saveExpense = mutation({
+  args: { description: v.string(), cost: v.number() },
+  handler: async (ctx, args) => {
+    const newExpense = await ctx.db.insert("expenses", {
+      description: args.description,
+      cost: args.cost,
+    });
+    return newExpense;
   },
 });
