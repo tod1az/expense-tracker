fd07d023e78f196f40698659fe371236cc71dd1f
tod1az <t.diaz.soto@gmail.com>
2024-03-11 11:39:16 -0300

    add stack navigation behavior

--- app/_layout.js
+++ app/_layout.js
@@ -1,16 +1,38 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { CircleUserRound } from "lucide-react-native";
 import { CONVEX_URL } from "@env";
 import "react-native-get-random-values";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router/stack";
+
+import { Text } from "react-native";
+import { useNavigation } from "expo-router";
 
 const convex = new ConvexReactClient(CONVEX_URL, {
   unsavedChangesWarning: false,
 });
 
 export default function Layout() {
+  const navigator = useNavigation();
   return (
     <ConvexProvider client={convex}>
-      <Slot />
+      <Stack
+        screenOptions={{
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerTitle: "Expense Tracker",
+          headerRight: () => (
+            <Text>
+              <CircleUserRound color={"black"} />
+            </Text>
+          ),
+        }}
+      />
     </ConvexProvider>
   );
 }
