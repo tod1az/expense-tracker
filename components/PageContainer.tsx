5c01a8c0fd10e853e55ac7ec193f60d1bf790eea
tod1az <t.diaz.soto@gmail.com>
2024-03-11 11:40:00 -0300

    delete margin top to the page container

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export default function PageContainer({ children }: { children: ReactNode }) {
   return (
-    <View className="bg-gray-600 h-screen mt-12 flex flex-col items-center">
+    <View className="bg-gray-600 h-screen  flex flex-col items-center">
       {children}
     </View>
   );
